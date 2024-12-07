76bcf8bcc6a8e9f67a1bfa61c9765ec179ac1181
nantaaditya <pramuditza@gmail.com>
2024-12-07 10:52:29 +0700

    feature: add internal api to execute db migration

---
+++ single-module-non-reactive/src/main/java/com/nantaaditya/example/api/internal/DatabaseController.java
@@ -0,0 +1,33 @@
+package com.nantaaditya.example.api.internal;
+
+import com.nantaaditya.example.model.constant.ResponseCode;
+import com.nantaaditya.example.model.response.Response;
+import com.nantaaditya.example.service.internal.DatabaseMigrationService;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.api.output.MigrateResult;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/internal-api/database")
+public class DatabaseController {
+
+  private final DatabaseMigrationService databaseMigrationService;
+
+  @PostMapping(value = "/migrate",
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public Response<MigrateResult> migrate() {
+    MigrateResult migrateResult = databaseMigrationService.migrate();
+    if (migrateResult.success) {
+      return Response.success(migrateResult);
+    }
+
+    return Response.failed(ResponseCode.INTERNAL_ERROR, Map.of("migration", List.of("failed")));
+  }
+}
