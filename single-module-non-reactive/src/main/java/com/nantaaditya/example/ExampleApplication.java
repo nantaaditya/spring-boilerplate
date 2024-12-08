135517e9e5f626f6cab2b8bad7e8ab6bdde79119
nantaaditya <pramuditza@gmail.com>
2024-12-08 08:45:06 +0700

    bugfix: add missing async configuration

--- single-module-non-reactive/src/main/java/com/nantaaditya/example/ExampleApplication.java
+++ single-module-non-reactive/src/main/java/com/nantaaditya/example/ExampleApplication.java
@@ -1,8 +1,15 @@
 package com.nantaaditya.example;
 
+import com.nantaaditya.example.properties.AsyncTaskProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableConfigurationProperties(value = {
+		AsyncTaskProperties.class
+})
 @SpringBootApplication
 public class ExampleApplication {
 
