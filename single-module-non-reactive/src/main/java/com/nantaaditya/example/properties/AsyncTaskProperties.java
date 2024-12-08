74a855d1fb89f9a81fc31e05b7f32a485b1ecb9b
nantaaditya <pramuditza@gmail.com>
2024-12-08 08:45:06 +0700

    bugfix: add missing async configuration

---
+++ single-module-non-reactive/src/main/java/com/nantaaditya/example/properties/AsyncTaskProperties.java
@@ -0,0 +1,26 @@
+package com.nantaaditya.example.properties;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("apps.async")
+public class AsyncTaskProperties {
+
+  private Map<String, Configuration> configurations = new HashMap<>();
+
+  @Data
+  public static class Configuration {
+    private int corePoolSize;
+    private int maxPoolSize;
+    private int queueCapacity;
+    private int keepAliveSeconds;
+    private String threadNamePrefix;
+  }
+
+  public Configuration getConfiguration(String name) {
+    return configurations.get(name);
+  }
+}
